f8613b3bbeddf7da5539618bfab83e84ad5df65a
gbrolo <gabrolo13@gmail.com>
2015-09-16 14:21:19 -0600

    Empezar a hacer el menu

--- core/src/com/piercystudio/PiercyGame.java
+++ core/src/com/piercystudio/PiercyGame.java
@@ -1,27 +1,57 @@
+/*
+ * PiercyGame.java
+ * 
+ * @author: G. Brolo
+ * 16/09/15
+ * 
+ * Crea el juego.
+ */
 package com.piercystudio;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
-public class PiercyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class PiercyGame extends Game {
+	private Stage myStage;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	public static int WIDTH = 480;
+	public static int HEIGHT = 320;
+	public static Content res;
 	
-	@Override
+	
 	public void create () {
+		myStage = new Stage();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, WIDTH, HEIGHT);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		res = new Content();
+		
+		setScreen(new MenuScreen(this));
+		
 	}
 
-	@Override
+
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+		myStage.act();
+		myStage.draw();
+	}
+	
+	public OrthographicCamera getCamera() {
+		return camera;
+	}
+
+	public SpriteBatch getBatch() {
+		return batch;
+	}
+
+	public Stage getMyStage() {
+		return myStage;
 	}
 }
