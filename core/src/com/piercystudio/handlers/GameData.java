5f3d1fa47976aee7995a0231f248cc62e63199b6
gbrolo <gabrolo13@gmail.com>
2015-10-23 21:01:41 -0600

    Bug fix: Console sintax crash

--- core/src/com/piercystudio/handlers/GameData.java
+++ core/src/com/piercystudio/handlers/GameData.java
@@ -35,6 +35,26 @@ public class GameData implements Serializable{
 		lvlErrors.put(4, 0);
 		lvlErrors.put(5, 0);
 		lvlErrors.put(6, 0);
+		lvlErrors.put(7, 0);
+		lvlErrors.put(8, 0);
+		lvlErrors.put(9, 0);
+		lvlErrors.put(10, 0);
+		lvlErrors.put(11, 0);
+		lvlErrors.put(12, 0);
+		lvlErrors.put(13, 0);
+		lvlErrors.put(14, 0);
+		lvlErrors.put(15, 0);
+		lvlErrors.put(16, 0);
+		lvlErrors.put(17, 0);
+		lvlErrors.put(18, 0);
+		lvlErrors.put(19, 0);
+		lvlErrors.put(20, 0);
+		lvlErrors.put(21, 0);
+		lvlErrors.put(22, 0);
+		lvlErrors.put(23, 0);
+		lvlErrors.put(24, 0);
+		lvlErrors.put(25, 0);
+		
 		
 	}
 	
